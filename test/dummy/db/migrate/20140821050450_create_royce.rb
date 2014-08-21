66eff1a240bf75a0c28034cad13fab03c509bf6e
Martin Nash <martin.j.nash@gmail.com>
2014-08-21 10:18:54 -0700

    Reved unused column on role table.

--- test/dummy/db/migrate/20140821050450_create_royce.rb
+++ test/dummy/db/migrate/20140821050450_create_royce.rb
@@ -5,13 +5,13 @@ class CreateRoyce < ActiveRecord::Migration
     create_table :royce_connector do |t|
       t.references :roleable, polymorphic: true, null: false
       t.references :role, null: false
+      t.timestamps
     end
 
     add_index :royce_connector, [:roleable_id, :roleable_type]
     add_index :royce_connector, :role_id
 
     create_table :royce_role do |t|
-      t.string :type
       t.string :name, null: false
       t.timestamps
     end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -14,16 +14,17 @@
 ActiveRecord::Schema.define(version: 20140821050450) do
 
   create_table "royce_connector", force: true do |t|
-    t.integer "roleable_id",   null: false
-    t.string  "roleable_type", null: false
-    t.integer "role_id",       null: false
+    t.integer  "roleable_id",   null: false
+    t.string   "roleable_type", null: false
+    t.integer  "role_id",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "royce_connector", ["role_id"], name: "index_royce_connector_on_role_id"
   add_index "royce_connector", ["roleable_id", "roleable_type"], name: "index_royce_connector_on_roleable_id_and_roleable_type"
 
   create_table "royce_role", force: true do |t|
-    t.string   "type"
     t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
